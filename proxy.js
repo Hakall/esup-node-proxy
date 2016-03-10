f81fdd7d36edcb83e2d6d3f8771ca244f0eb9e28
Hakall <abouskine@gmail.com>
2016-03-10 11:06:02 +0100

    errors handling

--- proxy.js
+++ proxy.js
@@ -4,7 +4,7 @@ var properties = require(process.cwd() + '/properties/properties');
 //
 // Create the HTTPS proxy server in front of a HTTP server
 //
-httpProxy.createServer({
+var proxy = httpProxy.createServer({
   target: {
     host: properties.esup.otp_api_address,
     port: properties.esup.otp_api_port
@@ -13,4 +13,15 @@ httpProxy.createServer({
     key: fs.readFileSync(process.cwd() + properties.esup.ssl_key, 'utf8'),
     cert: fs.readFileSync(process.cwd() + properties.esup.ssl_cert, 'utf8')
   }
-}).listen(properties.esup.proxy_port);
+});
+
+// Listen for the `error` event on `proxy`.
+proxy.on('error', function (err, req, res) {
+  res.writeHead(500, {
+    'Content-Type': 'text/plain'
+  });
+  console.log(JSON.stringify(err));
+  res.end('Something went wrong. '+JSON.stringify(err.code));
+});
+
+proxy.listen(properties.esup.proxy_port);
